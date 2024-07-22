6422bd9ff4e7d39f9a470a60eaff5173461f2b75
lyd <2428170173@qq.com>
2024-07-22 15:27:24 +0800

    dev 开发二手商品管理功能 lyd

---
+++ src/main/java/com/example/service/impl/GoodsServiceImpl.java
@@ -0,0 +1,87 @@
+package com.example.service.impl;
+
+import com.example.entity.Goods;
+import com.example.mapper.GoodsMapper;
+import com.example.service.GoodsService;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+/**
+ * @lyd
+ * @date 2024/7/22
+*/
+@Service
+public class GoodsServiceImpl implements GoodsService{
+
+    @Resource
+    private GoodsMapper goodsMapper;
+
+    /**
+     * 新增
+     */
+    @Override
+    public void add(Goods goods) {
+        goodsMapper.insert(goods);
+    }
+
+    /**
+     * 删除
+     */
+    @Override
+    public void deleteById(Integer id) {
+        goodsMapper.deleteById(id);
+    }
+
+    /**
+     * 批量删除
+     */
+    @Override
+    public void deleteBatch(List<Integer> ids) {
+        for (Integer id : ids) {
+            goodsMapper.deleteById(id);
+        }
+    }
+
+    /**
+     * 修改
+     */
+    @Override
+    public void updateById(Goods goods) {
+        goodsMapper.updateById(goods);
+    }
+
+    /**
+     * 根据ID查询
+     */
+    @Override
+    public Goods selectById(Integer id) {
+        return goodsMapper.selectById(id);
+    }
+
+    /**
+     * 查询所有
+     */
+    @Override
+    public List<Goods> selectAll(Goods goods) {
+        return goodsMapper.selectAll(goods);
+    }
+
+    /**
+     * 分页查询
+     */
+    @Override
+    public PageInfo<Goods> selectPage(Goods goods, Integer pageNum, Integer pageSize) {
+        PageHelper.startPage(pageNum, pageSize);
+        List<Goods> list = goodsMapper.selectAll(goods);
+        return PageInfo.of(list);
+    }
+
+}
+
+
+
+
