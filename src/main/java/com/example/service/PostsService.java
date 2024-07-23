5da5d0338c50b5b18c6594651d951f16e3393b37
lyd <2428170173@qq.com>
2024-07-23 09:32:23 +0800

    dev 开发社区帖子管理功能 lyd

---
+++ src/main/java/com/example/service/PostsService.java
@@ -0,0 +1,48 @@
+package com.example.service;
+
+import com.example.entity.Posts;
+import com.github.pagehelper.PageInfo;
+
+import java.util.List;
+
+/**
+ * @lyd
+ * @date 2024/7/22
+*/
+public interface PostsService {
+
+    /**
+     * 新增
+     */
+    void add(Posts posts);
+
+    /**
+     * 删除
+     */
+    void deleteById(Integer id);
+
+    /**
+     * 批量删除
+     */
+    void deleteBatch(List<Integer> ids);
+
+    /**
+     * 修改
+     */
+    void updateById(Posts posts);
+
+    /**
+     * 根据ID查询
+     */
+    Posts selectById(Integer id);
+
+    /**
+     * 查询所有
+     */
+    List<Posts> selectAll(Posts posts);
+
+    /**
+     * 分页查询
+     */
+    PageInfo<Posts> selectPage(Posts posts, Integer pageNum, Integer pageSize);
+}
